f4b3571453f081b5ef901543d3ba68e26947002c
fe-lam-dinh <dinhtronglam172000@gmail.com>
2023-02-18 20:54:49 +0700

    create folders and packages

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+//init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+//init db
+
+//init routers
+app.get("/", (req, res, next) => {
+  const strCompress = "hello word!";
+  return res.status(200).json({
+    message: "Welcome Lam",
+    metaData: strCompress.repeat(10000),
+  });
+});
+
+//handing error
+
+module.exports = app;
